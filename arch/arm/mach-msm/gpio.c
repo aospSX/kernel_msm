kernelzilla/android-kernel

msm: enhance error handling in display power functions
Board-specific power function signatures are changed. Functions now
have a return type so that error codes can be propagated to display
drivers.

Change-Id: I53c4242bbab2405c46b4fdf2c3ca6097cf70a7da
Signed-off-by: Bobby Crabtree <bobbyc@codeaurora.org>

arch/arm/mach-msm/gpio.c
@@ -683,15 +683,16 @@ err:
 }
 EXPORT_SYMBOL(msm_gpios_enable);
 
-void msm_gpios_disable(const struct msm_gpio *table, int size)
+int msm_gpios_disable(const struct msm_gpio *table, int size)
 {
-	int rc;
+	int rc = 0;
 	int i;
 	const struct msm_gpio *g;
 	for (i = size-1; i >= 0; i--) {
+		int tmp;
 		g = table + i;
-		rc = gpio_tlmm_config(g->gpio_cfg, GPIO_DISABLE);
-		if (rc) {
+		tmp = gpio_tlmm_config(g->gpio_cfg, GPIO_DISABLE);
+		if (tmp) {
 			pr_err("gpio_tlmm_config(0x%08x, GPIO_DISABLE)"
 			       " <%s> failed: %d\n",
 			       g->gpio_cfg, g->label ?: "?", rc);
@@ -699,8 +700,12 @@ void msm_gpios_disable(const struct msm_gpio *table, int size)
 			       GPIO_PIN(g->gpio_cfg), GPIO_FUNC(g->gpio_cfg),
 			       GPIO_DIR(g->gpio_cfg), GPIO_PULL(g->gpio_cfg),
 			       GPIO_DRVSTR(g->gpio_cfg));
+			if (!rc)
+				rc = tmp;
 		}
 	}
+
+	return rc;
 }
 EXPORT_SYMBOL(msm_gpios_disable);
 

aospSX/kernel_msm

arch/arm/mach-msm/gpio.c
@@ -741,15 +741,16 @@ err:
 }
 EXPORT_SYMBOL(msm_gpios_enable);
 
-void msm_gpios_disable(const struct msm_gpio *table, int size)
+int msm_gpios_disable(const struct msm_gpio *table, int size)
 {
-	int rc;
+	int rc = 0;
 	int i;
 	const struct msm_gpio *g;
 	for (i = size-1; i >= 0; i--) {
+		int tmp;
 		g = table + i;
-		rc = gpio_tlmm_config(g->gpio_cfg, GPIO_CFG_DISABLE);
-		if (rc) {
+		tmp = gpio_tlmm_config(g->gpio_cfg, GPIO_CFG_DISABLE);
+		if (tmp) {
 			pr_err("gpio_tlmm_config(0x%08x, GPIO_CFG_DISABLE)"
 			       " <%s> failed: %d\n",
 			       g->gpio_cfg, g->label ?: "?", rc);
@@ -757,8 +758,12 @@ void msm_gpios_disable(const struct msm_gpio *table, int size)
 			       GPIO_PIN(g->gpio_cfg), GPIO_FUNC(g->gpio_cfg),
 			       GPIO_DIR(g->gpio_cfg), GPIO_PULL(g->gpio_cfg),
 			       GPIO_DRVSTR(g->gpio_cfg));
+			if (!rc)
+				rc = tmp;
 		}
 	}
+
+	return rc;
 }
 EXPORT_SYMBOL(msm_gpios_disable);
 